ed836ba17eee53e051ccf46795f4e19261221ce2
anstepp <stepp.aaron@gmail.com>
2019-09-15 11:44:55 -0400

    added beat and measure functionality

---
+++ py2musicxml/beat.py
@@ -0,0 +1,4 @@
+class Beat:
+
+	def __init__(self):
+		pass
--- py2musicxml/measure.py
+++ py2musicxml/measure.py
@@ -1,8 +1,26 @@
 class Measure():
 	index = None
 	notes = []
+	weight = None
+	subdivisions = None 
+	meter = []
+	time_signature = []
 
-	def __init__(self):
-		pass
+	def __init__(self, notes: Iterable[NoteList]):
+		self.get_subdivisions()
+		self.notes = notes
 
-	
+	def get_subdivisions(self, factor: int, divisions: int):
+		time_factor = factor
+		smallest_division = divisions
+		self.subdivisions = factor * divsions
+
+	def divide_measure(self):
+		current_count = 0
+		current_beat = 0
+		measure_map = []
+		for beat in self.meter:
+			measure_map.append(beat * self.subdivisions)
+		for current_note in self.notes:
+			current_count += current_note.dur
+			if current_count > 
