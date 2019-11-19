062c7a13faff070ccc34ad816d8eef535dcbf7bd
anstepp <stepp.aaron@gmail.com>
2019-11-18 22:06:05 -0500

    added logic for whole measure rest

--- py2musicxml/rest.py
+++ py2musicxml/rest.py
@@ -3,3 +3,7 @@ class Rest:
     def __init__(self, duration):
 
         self.dur = duration
+        if self.dur > 0:
+            self.is_measure = True
+        else:
+            self.is_measure = False
