88f158b64437ed884add75e411778853b30d4947
Rachel House <rachhouse@gmail.com>
2019-12-09 22:12:09 -0500

    separate py2musicxml lib folder into notation and analysis subfolders

--- py2musicxml/analysis/Tree.py
+++ py2musicxml/analysis/Tree.py
@@ -1,10 +1,9 @@
-import Node.py
-import notelist.py
+from .Node import Node
 
 class Tree:
     nodeList = []
 
-    def __init__(self, notelist):
+    def __init__(self):
         pass
 
     def makeTree(self):
