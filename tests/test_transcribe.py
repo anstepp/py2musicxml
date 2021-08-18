ffaa1ed7f430b76bc9fb74a5335e3c0c4b5eda8d
anstepp <stepp.aaron@gmail.com>
2021-08-17 21:41:17 -0400

    can i haz wav filz

--- tests/test_transcribe.py
+++ tests/test_transcribe.py
@@ -1,3 +1,8 @@
+import pathlib
+from distutils import dir_util
+
+from numpy.lib.npyio import load
+
 import pytest
 
 import numpy as np
@@ -8,6 +13,18 @@ from py2musicxml.notation import Note, Part, Score, Tempo
 from py2musicxml.analysis import AutoTranscribe
 
 peak = namedtuple("peak", ["bin", "freq", "amp", "dur"])
+
+SAMPLE_AUDIO_DIR = pathlib.Path(__file__).parent / "sample_audio"
+
+@pytest.fixture
+def load_sample_audio(tmp_path):
+
+    # Copy sample audio files into pytest temp directory.
+    temp_sample_audio = tmp_path / "sample_audio"
+    temp_sample_audio.mkdir()
+    dir_util.copy_tree(str(SAMPLE_AUDIO_DIR), str(temp_sample_audio))
+    return temp_sample_audio
+
 
 @pytest.fixture
 def a_440():
@@ -45,27 +62,24 @@ def test_parabolic(basic_at, a_440):
             assert peak.freq > 0
             assert peak.amp < 0
 
-def test_supply_audio(basic_tempo):
+def test_supply_audio(basic_tempo, load_sample_audio):
 
     at = AutoTranscribe(1024, basic_tempo)
 
-    at._supply_audio("test_audio/sine440.wav")
+    at._supply_audio(load_sample_audio / "sine440.wav")
 
 def test_get_pitch(basic_at):
     assert basic_at._get_pitch(259) == (4,0)
     assert basic_at._get_pitch(440) == (4,9)
 
-def test_two_way_mismatch(basic_at):
-    basic_at._two_way_mismatch
-
-def test_fft_one_pitch():
+def test_fft_one_pitch(load_sample_audio):
     
     N = 2048
     auto_transcribe = AutoTranscribe(N, Tempo(60, 1))
 
     #print(auto_transcribe.__dict__)
 
-    auto_transcribe._supply_audio("test_audio/violinclip1.wav")
+    auto_transcribe._supply_audio(load_sample_audio / "violinclip1.wav")
 
     f0_range = (24, 48)
 
@@ -76,12 +90,12 @@ def test_fft_one_pitch():
     print(len(resulting_pitches))
     assert Note(0.88, 4, 3) == resulting_pitches[0]
 
-def test_smooth_notes():
+def test_smooth_notes(load_sample_audio):
     
     N = 2048
     auto_transcribe = AutoTranscribe(N, Tempo(60,1))
 
-    auto_transcribe._supply_audio("test_audio/violinclip1.wav")
+    auto_transcribe._supply_audio(load_sample_audio / "violinclip1.wav")
 
     f0_range = (24, 48)
 
@@ -93,7 +107,7 @@ def test_smooth_notes():
 
     auto_transcribe_2 = AutoTranscribe(N, Tempo(60,1))
 
-    auto_transcribe_2._supply_audio("test_audio/y2monoChunk.wav")
+    auto_transcribe_2._supply_audio("tests/sample_audio/y2monoChunk.wav")
 
     f0_range = (32, 48)
 
